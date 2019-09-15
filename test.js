6a4f1b9e986f4d3a74b7675b299eb143ccc52a2c
Moritz <hess@BEMB-IMBI-175.ad.uniklinik-freiburg.de>
2019-09-15 20:08:09 +0200

    new examples

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
    }
+
+function hover(element) {
+// element.setAttribute('src', 'kermitpiggy.jpg');
+element.style.setAttribute('width', '600px');
+element.style.setAttribute('height', '600px');
+}
+
+function unhover(element) {
+// <element.setAttribute('src', 'kermit.jpg');
+element.style.setAttribute('width', '200px');
+element.style.setAttribute('height', '200px');
+}
